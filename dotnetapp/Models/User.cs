3ba91d013cb17d06d3c5d20f8d48c36531186b6c
vinaykumar40110917 <vinaykumarnaidu5558@gmail.com>
2025-04-26 09:12:52 +0000

    Models by vinay

---
+++ dotnetapp/Models/User.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnetapp.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId {get;set;}
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email {get;set;}
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password {get;set;}
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username {get;set;}
+
+        [Required(ErrorMessage = "Mobile number is required.")]
+        public string MobileNumber {get;set;}
+
+        [Required(ErrorMessage = "User role is required.")]
+        public string UserRole {get;set;}
+
+    }
+}
