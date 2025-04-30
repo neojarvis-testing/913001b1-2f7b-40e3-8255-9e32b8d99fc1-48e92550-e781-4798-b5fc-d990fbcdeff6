8990c8a83f2b45eb108fa9f4422322e04659bdcb
Grjrahul <Grjrahulx3@gmail.com>
2025-04-30 04:49:48 +0000

    Customer and manager Nav

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
-
+import { RouterModule, Routes } from '@angular/router';
+ 
+import { LoginComponent } from './components/login/login.component';
+import { ManagernavComponent } from './components/managernav/managernav.component';
+import { HomeComponent } from './components/home/home.component';
+import { ManagerviewallaccountsComponent } from './components/managerviewallaccounts/managerviewallaccounts.component';
+import { ManagerviewalltransactionsComponent } from './components/managerviewalltransactions/managerviewalltransactions.component';
+import { ManagerviewfeedbackComponent } from './components/managerviewfeedback/managerviewfeedback.component';
+ 
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+ 
+  // Manager navigation
+  {
+    path: 'manager',
+    component: ManagernavComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'accounts', component: ManagerviewallaccountsComponent },
+      { path: 'transactions', component: ManagerviewalltransactionsComponent },
+      { path: 'feedbacks', component: ManagerviewfeedbackComponent }
+    ]
+  },
+];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
--- angularapp/src/app/components/customernav/customernav.component.ts
+++ angularapp/src/app/components/customernav/customernav.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+ 
 @Component({
   selector: 'app-customernav',
   templateUrl: './customernav.component.html',
   styleUrls: ['./customernav.component.css']
 })
 export class CustomernavComponent implements OnInit {
-
-  constructor() { }
-
+ 
+  constructor(private router: Router) {}
+ 
   ngOnInit(): void {
+    // Optionally check for valid login or role here
+    const role = localStorage.getItem('role');
+    if (role !== 'customer') {
+      this.router.navigate(['/login']);
+    }
   }
-
+ 
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
+ 
